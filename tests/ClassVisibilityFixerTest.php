8b7bdbe700d69a13e84bcd54a381177f19454bd6
Kenny1911 <o-muzyka@mail.ru>
2025-03-24 23:51:36 +0400

    Add test to class, that has license text

--- tests/ClassVisibilityFixerTest.php
+++ tests/ClassVisibilityFixerTest.php
@@ -564,6 +564,37 @@ final class ClassVisibilityFixerTest extends TestCase
         );
     }
 
+    public function testFileContainsLicenseInformation(): void
+    {
+        $this->doTest(
+            <<<PHP
+            namespace Some\Namespace;
+            
+            /*
+             * This source file is subject to the MIT license that is bundled
+             * with this source code in the file LICENSE.
+             */
+
+            /**
+             * @internal
+             * @psalm-internal Some\Namespace
+             */
+            class Foo {}
+            PHP,
+            <<<PHP
+            namespace Some\Namespace;
+            
+            /*
+             * This source file is subject to the MIT license that is bundled
+             * with this source code in the file LICENSE.
+             */
+
+            class Foo {}
+            PHP,
+            [],
+        );
+    }
+
     /**
      * @param non-empty-string $expected
      * @param non-empty-string $code
